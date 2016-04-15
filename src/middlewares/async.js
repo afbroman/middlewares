70a3eaccadb4812a6b474d91e5793400355f59d7
Andrew Broman <broman@acm.org>
2016-04-14 20:59:30 -0500

    Implement ajax middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,15 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
-
-    next(action);
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
   };
+
+  action.payload
+  .then(response => {
+    const newAction = { ...action, payload: response };
+
+    // dispatch sends the action through all the middlewares again
+    dispatch(newAction);
+  });
 }
